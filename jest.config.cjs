7ee2c92b9760af3fee785a1578b41a8a68520527
andreisoroka <unickpro@gmail.com>
2023-05-15 21:31:49 +0300

    get paths from tsconfig for coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,37 @@
+const JSON5 = require('json5');
+const fs = require('fs');
+const path = require('path');
+
+const tsConfig = JSON5.parse(fs.readFileSync('./tsconfig.json').toString());
+
+/**
+ * Converts TypeScript path mappings to Jest moduleNameMapper format.
+ * Because "@swc/jest" not supported aliases from ts config in 2023
+ *
+ * @returns {Object} An object where keys are paths in Jest format and values are corresponding paths in TypeScript format.
+ *
+ * Example:
+ * Converts from:
+ * "@/*": ["./src/*"]
+ * To:
+ * { '@/(.*)': '<rootDir>/src/$1' }
+ */
+function makeModuleNameMapper() {
+  const { paths } = tsConfig.compilerOptions;
+
+  return Object.entries(paths).reduce((aliases, [key, val]) => {
+    const newKey = key.replace('/*', '/(.*)');
+    const templatePath = val[0].replace('/*', '/$1');
+    aliases[newKey] = path.join('<rootDir>', templatePath);
+    return aliases;
+  }, {});
+}
+
 module.exports = {
   coverageReporters: ["html"],
   coverageDirectory: 'dist/coverage',
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
   },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
+  'moduleNameMapper': makeModuleNameMapper()
 };

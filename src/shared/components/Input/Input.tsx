6dca0fe846454582e82cc6fb74c9e731387a62f1
andreisoroka <unickpro@gmail.com>
2023-05-22 10:19:43 +0300

    wrap Input and Select to memo

--- src/shared/components/Input/Input.tsx
+++ src/shared/components/Input/Input.tsx
@@ -1,37 +1,41 @@
 import type { ForwardedRef } from "react";
-import React, { forwardRef } from "react";
+import React, { forwardRef, memo } from "react";
 import InputStyle from "./input.module.scss";
 import type InputProps from "./types/InputProps.type";
 
-export const Input = forwardRef(
-  (
-    {
-      value,
-      onChange,
-      placeholder,
-      required = false,
-      maxlength = 255,
-      type = "text",
-      dataTestId,
-    }: InputProps,
-    ref: ForwardedRef<HTMLInputElement>
-  ) => {
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      onChange(event.target.value);
-    };
+export const Input = memo(
+  forwardRef(
+    (
+      {
+        value,
+        onChange,
+        placeholder,
+        required = false,
+        maxlength = 255,
+        type = "text",
+        dataTestId,
+      }: InputProps,
+      ref: ForwardedRef<HTMLInputElement>
+    ) => {
+      const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+      ) => {
+        onChange(event.target.value);
+      };
 
-    return (
-      <input
-        ref={ref}
-        type={type}
-        value={value}
-        onChange={handleInputChange}
-        placeholder={placeholder}
-        className={InputStyle.input}
-        required={required}
-        maxLength={maxlength}
-        data-testid={dataTestId}
-      />
-    );
-  }
+      return (
+        <input
+          ref={ref}
+          type={type}
+          value={value}
+          onChange={handleInputChange}
+          placeholder={placeholder}
+          className={InputStyle.input}
+          required={required}
+          maxLength={maxlength}
+          data-testid={dataTestId}
+        />
+      );
+    }
+  )
 );
